11587a92327e4db562aa0fcafefc7067d2cb3144
Leonardo <leonardo.ciaccio@gmail.com>
2020-03-23 11:09:55 +0100

    Aggiungere i file di progetto.

---
+++ ZigZag/ZigZag.cs
@@ -0,0 +1,476 @@
+﻿/*  CTRADER GURU --> Template 1.0.3
+
+    Homepage    : https://ctrader.guru/
+    Telegram    : https://t.me/ctraderguru
+    Twitter     : https://twitter.com/cTraderGURU/
+    Facebook    : https://www.facebook.com/ctrader.guru/
+    YouTube     : https://www.youtube.com/channel/UCKkgbw09Fifj65W5t5lHeCQ
+    GitHub      : https://github.com/cTraderGURU/
+    TOS         : https://ctrader.guru/termini-del-servizio/
+
+*/
+
+using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Specialized;
+using cAlgo.API;
+
+// --> Microsoft Visual Studio 2017 --> Strumenti --> Gestione pacchetti NuGet --> Gestisci pacchetti NuGet per la soluzione... --> Installa
+using Newtonsoft.Json;
+
+
+
+namespace cAlgo.Indicators
+{
+
+    // --> AccessRights = AccessRights.FullAccess se si vuole controllare gli aggiornamenti
+    [Indicator(IsOverlay = true, AccessRights = AccessRights.FullAccess)]
+    public class ZigZag : Indicator
+    {
+
+        #region Enums
+
+        // --> Eventuali enumeratori li mettiamo qui
+
+        #endregion
+
+        #region Identity
+
+        /// <summary>
+        /// ID prodotto, identificativo, viene fornito da ctrader.guru, 74909 è il riferimento del template in uso
+        /// </summary>
+        public const int ID = 60815;
+
+        /// <summary>
+        /// Nome del prodotto, identificativo, da modificare con il nome della propria creazione
+        /// </summary>
+        public const string NAME = "ZigZag";
+
+        /// <summary>
+        /// La versione del prodotto, progressivo, utilie per controllare gli aggiornamenti se viene reso disponibile sul sito ctrader.guru
+        /// </summary>
+        public const string VERSION = "1.0.1";
+
+        #endregion
+
+        #region Params
+
+        /// <summary>
+        /// Identità del prodotto nel contesto di ctrader.guru
+        /// </summary>
+        [Parameter(NAME + " " + VERSION, Group = "Identity", DefaultValue = "https://ctrader.guru/product/zigzag/")]
+        public string ProductInfo { get; set; }
+
+        [Parameter(DefaultValue = 12, Group = "Params")]
+        public int Depth { get; set; }
+
+        [Parameter(DefaultValue = 5, Group = "Params")]
+        public int Deviation { get; set; }
+
+        [Parameter(DefaultValue = 3, Group = "Params")]
+        public int BackStep { get; set; }
+
+        [Output("ZigZag", LineColor = "DodgerBlue", LineStyle = LineStyle.Lines)]
+        public IndicatorDataSeries Result { get; set; }
+
+        #endregion
+
+        #region Property
+
+        private double _lastLow;
+        private double _lastHigh;
+        private double _low;
+        private double _high;
+        private int _lastHighIndex;
+        private int _lastLowIndex;
+        private int _type;
+        private double _point;
+        private double _currentLow;
+        private double _currentHigh;
+
+        private IndicatorDataSeries _highZigZags;
+        private IndicatorDataSeries _lowZigZags;
+
+        #endregion
+
+        #region Indicator Events
+
+        protected override void Initialize()
+        {
+
+            // --> Stampo nei log la versione corrente
+            Print("{0} : {1}", NAME, VERSION);
+
+            // --> Se viene settato l'ID effettua un controllo per verificare eventuali aggiornamenti
+            _checkProductUpdate();
+
+            _highZigZags = CreateDataSeries();
+            _lowZigZags = CreateDataSeries();
+            _point = Symbol.TickSize;
+
+        }
+
+        public override void Calculate(int index)
+        {
+
+            if (index < Depth)
+            {
+                Result[index] = 0;
+                _highZigZags[index] = 0;
+                _lowZigZags[index] = 0;
+                return;
+            }
+
+            _currentLow = Functions.Minimum(Bars.LowPrices, Depth);
+
+            if (Math.Abs(_currentLow - _lastLow) < double.Epsilon)
+                _currentLow = 0.0;
+            else
+            {
+
+                _lastLow = _currentLow;
+
+                if ((Bars.LowPrices[index] - _currentLow) > (Deviation * _point))
+                    _currentLow = 0.0;
+                else
+                {
+                    for (int i = 1; i <= BackStep; i++)
+                    {
+                        if (Math.Abs(_lowZigZags[index - i]) > double.Epsilon && _lowZigZags[index - i] > _currentLow)
+                            _lowZigZags[index - i] = 0.0;
+                    }
+                }
+            }
+            if (Math.Abs(Bars.LowPrices[index] - _currentLow) < double.Epsilon)
+                _lowZigZags[index] = _currentLow;
+            else
+                _lowZigZags[index] = 0.0;
+
+            _currentHigh = Bars.HighPrices.Maximum(Depth);
+
+            if (Math.Abs(_currentHigh - _lastHigh) < double.Epsilon)
+                _currentHigh = 0.0;
+            else
+            {
+
+                _lastHigh = _currentHigh;
+
+                if ((_currentHigh - Bars.HighPrices[index]) > (Deviation * _point))
+                    _currentHigh = 0.0;
+                else
+                {
+                    for (int i = 1; i <= BackStep; i++)
+                    {
+                        if (Math.Abs(_highZigZags[index - i]) > double.Epsilon && _highZigZags[index - i] < _currentHigh)
+                            _highZigZags[index - i] = 0.0;
+                    }
+                }
+            }
+
+            if (Math.Abs(Bars.HighPrices[index] - _currentHigh) < double.Epsilon)
+                _highZigZags[index] = _currentHigh;
+            else
+                _highZigZags[index] = 0.0;
+
+
+            switch (_type)
+            {
+                case 0:
+                    if (Math.Abs(_low - 0) < double.Epsilon && Math.Abs(_high - 0) < double.Epsilon)
+                    {
+                        if (Math.Abs(_highZigZags[index]) > double.Epsilon)
+                        {
+                            _high = Bars.HighPrices[index];
+                            _lastHighIndex = index;
+                            _type = -1;
+                            Result[index] = _high;
+                        }
+                        if (Math.Abs(_lowZigZags[index]) > double.Epsilon)
+                        {
+                            _low = Bars.LowPrices[index];
+                            _lastLowIndex = index;
+                            _type = 1;
+                            Result[index] = _low;
+                        }
+                    }
+                    break;
+                case 1:
+                    if (Math.Abs(_lowZigZags[index]) > double.Epsilon && _lowZigZags[index] < _low && Math.Abs(_highZigZags[index] - 0.0) < double.Epsilon)
+                    {
+                        Result[_lastLowIndex] = double.NaN;
+                        _lastLowIndex = index;
+                        _low = _lowZigZags[index];
+                        Result[index] = _low;
+                    }
+                    if (Math.Abs(_highZigZags[index] - 0.0) > double.Epsilon && Math.Abs(_lowZigZags[index] - 0.0) < double.Epsilon)
+                    {
+                        _high = _highZigZags[index];
+                        _lastHighIndex = index;
+                        Result[index] = _high;
+                        _type = -1;
+                    }
+                    break;
+                case -1:
+                    if (Math.Abs(_highZigZags[index]) > double.Epsilon && _highZigZags[index] > _high && Math.Abs(_lowZigZags[index] - 0.0) < double.Epsilon)
+                    {
+                        Result[_lastHighIndex] = double.NaN;
+                        _lastHighIndex = index;
+                        _high = _highZigZags[index];
+                        Result[index] = _high;
+                    }
+                    if (Math.Abs(_lowZigZags[index]) > double.Epsilon && Math.Abs(_highZigZags[index]) < double.Epsilon)
+                    {
+                        _low = _lowZigZags[index];
+                        _lastLowIndex = index;
+                        Result[index] = _low;
+                        _type = 1;
+                    }
+                    break;
+                default:
+                    return;
+            }
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Effettua un controllo sul sito ctrader.guru per mezzo delle API per verificare la presenza di aggiornamenti, solo in realtime
+        /// </summary>
+        private void _checkProductUpdate()
+        {
+
+            // --> Controllo solo se solo in realtime, evito le chiamate in backtest
+            if (RunningMode != RunningMode.RealTime)
+                return;
+
+            // --> Organizzo i dati per la richiesta degli aggiornamenti
+            Guru.API.RequestProductInfo Request = new Guru.API.RequestProductInfo 
+            {
+
+                MyProduct = new Guru.Product 
+                {
+
+                    ID = ID,
+                    Name = NAME,
+                    Version = VERSION
+
+                },
+                AccountBroker = Account.BrokerName,
+                AccountNumber = Account.Number
+
+            };
+
+            // --> Effettuo la richiesta
+            Guru.API Response = new Guru.API(Request);
+
+            // --> Controllo per prima cosa la presenza di errori di comunicazioni
+            if (Response.ProductInfo.Exception != "")
+            {
+
+                Print("{0} Exception : {1}", NAME, Response.ProductInfo.Exception);
+
+            }
+            // --> Chiedo conferma della presenza di nuovi aggiornamenti
+            else if (Response.HaveNewUpdate())
+            {
+
+                string updatemex = string.Format("{0} : Updates available {1} ( {2} )", NAME, Response.ProductInfo.LastProduct.Version, Response.ProductInfo.LastProduct.Updated);
+
+                // --> Informo l'utente con un messaggio sul grafico e nei log del cbot
+                Chart.DrawStaticText(NAME + "Updates", updatemex, VerticalAlignment.Top, HorizontalAlignment.Left, Color.Red);
+                Print(updatemex);
+
+            }
+
+        }
+
+        #endregion
+
+
+    }
+
+}
+
+/// <summary>
+/// NameSpace che racchiude tutte le feature ctrader.guru
+/// </summary>
+namespace Guru
+{
+    /// <summary>
+    /// Classe che definisce lo standard identificativo del prodotto nel marketplace ctrader.guru
+    /// </summary>
+    public class Product
+    {
+
+        public int ID = 0;
+        public string Name = "";
+        public string Version = "";
+        public string Updated = "";
+
+    }
+
+    /// <summary>
+    /// Offre la possibilità di utilizzare le API messe a disposizione da ctrader.guru per verificare gli aggiornamenti del prodotto.
+    /// Permessi utente "AccessRights = AccessRights.FullAccess" per accedere a internet ed utilizzare JSON
+    /// </summary>
+    public class API
+    {
+        /// <summary>
+        /// Costante da non modificare, corrisponde alla pagina dei servizi API
+        /// </summary>
+        private const string Service = "https://ctrader.guru/api/product_info/";
+
+        /// <summary>
+        /// Costante da non modificare, utilizzata per filtrare le richieste
+        /// </summary>
+        private const string UserAgent = "cTrader Guru";
+
+        /// <summary>
+        /// Variabile dove verranno inserite le direttive per la richiesta
+        /// </summary>
+        private RequestProductInfo RequestProduct = new RequestProductInfo();
+
+        /// <summary>
+        /// Variabile dove verranno inserite le informazioni identificative dal server dopo l'inizializzazione della classe API
+        /// </summary>
+        public ResponseProductInfo ProductInfo = new ResponseProductInfo();
+
+        /// <summary>
+        /// Classe che formalizza i parametri di richiesta, vengono inviate le informazioni del prodotto e di profilazione a fini statistici
+        /// </summary>
+        public class RequestProductInfo
+        {
+
+            /// <summary>
+            /// Il prodotto corrente per il quale richiediamo le informazioni
+            /// </summary>
+            public Product MyProduct = new Product();
+
+            /// <summary>
+            /// Broker con il quale effettiamo la richiesta
+            /// </summary>
+            public string AccountBroker = "";
+
+            /// <summary>
+            /// Il numero di conto con il quale chiediamo le informazioni
+            /// </summary>
+            public int AccountNumber = 0;
+
+        }
+
+        /// <summary>
+        /// Classe che formalizza lo standard per identificare le informazioni del prodotto
+        /// </summary>
+        public class ResponseProductInfo
+        {
+
+            /// <summary>
+            /// Il prodotto corrente per il quale vengono fornite le informazioni
+            /// </summary>
+            public Product LastProduct = new Product();
+
+            /// <summary>
+            /// Eccezioni in fase di richiesta al server, da utilizzare per controllare l'esito della comunicazione
+            /// </summary>
+            public string Exception = "";
+
+            /// <summary>
+            /// La risposta del server
+            /// </summary>
+            public string Source = "";
+
+        }
+
+        /// <summary>
+        /// Richiede le informazioni del prodotto richiesto
+        /// </summary>
+        /// <param name="Request"></param>
+        public API(RequestProductInfo Request)
+        {
+
+            RequestProduct = Request;
+
+            // --> Non controllo se non ho l'ID del prodotto
+            if (Request.MyProduct.ID <= 0)
+                return;
+
+            // --> Dobbiamo supervisionare la chiamata per registrare l'eccexione
+            try
+            {
+
+                // --> Strutturo le informazioni per la richiesta POST
+                NameValueCollection data = new NameValueCollection 
+                {
+                    {
+                        "account_broker",
+                        Request.AccountBroker
+                    },
+                    {
+                        "account_number",
+                        Request.AccountNumber.ToString()
+                    },
+                    {
+                        "my_version",
+                        Request.MyProduct.Version
+                    },
+                    {
+                        "productid",
+                        Request.MyProduct.ID.ToString()
+                    }
+                };
+
+                // --> Autorizzo tutte le pagine di questo dominio
+                Uri myuri = new Uri(Service);
+                string pattern = string.Format("{0}://{1}/.*", myuri.Scheme, myuri.Host);
+
+                Regex urlRegEx = new Regex(pattern);
+                WebPermission p = new WebPermission(NetworkAccess.Connect, urlRegEx);
+                p.Assert();
+
+                // --> Protocollo di sicurezza https://
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)192 | (SecurityProtocolType)768 | (SecurityProtocolType)3072;
+
+                // -->> Richiedo le informazioni al server
+                using (var wb = new WebClient())
+                {
+
+                    wb.Headers.Add("User-Agent", UserAgent);
+
+                    var response = wb.UploadValues(myuri, "POST", data);
+                    ProductInfo.Source = Encoding.UTF8.GetString(response);
+
+                }
+
+                // -->>> Nel cBot necessita l'attivazione di "AccessRights = AccessRights.FullAccess"
+                ProductInfo.LastProduct = JsonConvert.DeserializeObject<Product>(ProductInfo.Source);
+
+            } catch (Exception Exp)
+            {
+
+                // --> Qualcosa è andato storto, registro l'eccezione
+                ProductInfo.Exception = Exp.Message;
+
+            }
+
+        }
+
+        /// <summary>
+        /// Esegue un confronto tra le versioni per determinare la presenza di aggiornamenti
+        /// </summary>
+        /// <returns></returns>
+        public bool HaveNewUpdate()
+        {
+
+            // --> Voglio essere sicuro che stiamo lavorando con le informazioni giuste
+            return (ProductInfo.LastProduct.ID == RequestProduct.MyProduct.ID && ProductInfo.LastProduct.Version != "" && RequestProduct.MyProduct.Version != "" && new Version(RequestProduct.MyProduct.Version).CompareTo(new Version(ProductInfo.LastProduct.Version)) < 0);
+
+        }
+
+    }
+
+}
